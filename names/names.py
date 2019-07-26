b00a15c453ae19fc88d3147ffe7432c4fb3eb1ba
Anubhav311 <anubhavbhambri1@gmail.com>
2019-07-26 21:47:43 +0530

    solved problem in names.py

--- names/names.py
+++ names/names.py
@@ -11,10 +11,29 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+namesDict = {}
+
+## Already provided solution
+## Found 64 duplicates
+## Runtime: 14 seconds
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+
+# MVP Solution
+# Found 64 duplicates
+# Runtime: 0.0129 seconds
+for i in range(len(names_1)):
+        namesDict[names_1[i]] = 0
+
+for j in range(len(names_2)):
+        if names_2[j] in namesDict:
+                duplicates.append(names_2[j])
+
+
+
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
