eb6c17a555e42c1fc8a290d089ebc963c99cd4f2
Anubhav311 <anubhavbhambri1@gmail.com>
2019-07-26 23:17:00 +0530

    did stretch problem in names.py

--- names/names.py
+++ names/names.py
@@ -13,13 +13,13 @@ f.close()
 duplicates = []
 namesDict = {}
 
-## Already provided solution
-## Found 64 duplicates
-## Runtime: 14 seconds
-# for name_1 in names_1:
-#     for name_2 in names_2:
-#         if name_1 == name_2:
-#             duplicates.append(name_1)
+# Already provided solution
+# Found 64 duplicates
+# Runtime: 14 seconds
+for name_1 in names_1:
+    for name_2 in names_2:
+        if name_1 == name_2:
+            duplicates.append(name_1)
 
 
 # MVP Solution
@@ -32,6 +32,38 @@ for j in range(len(names_2)):
         if names_2[j] in namesDict:
                 duplicates.append(names_2[j])
 
+
+# Stretch Solution
+# Found 64 duplicates
+# Runtime: 0.151 seconds
+names_2.sort()
+
+def binary_search(arr, target):        
+        if len(arr) == 0:
+                return -1
+        
+        low = 0
+        high = len(arr)-1
+        targetFound = False
+        
+        while not targetFound:
+                average = round((low + high) / 2)
+                if arr[average] == target:
+                        targetFound = True
+                        return arr[average]
+                elif high - low == 1:
+                        break
+                elif arr[average] > target:
+                        high = average
+                else:
+                        low = average
+
+for name in names_1:
+        result = binary_search(names_2, name)
+        if result == None:
+                pass
+        else:
+                duplicates.append(result)
 
 
 
